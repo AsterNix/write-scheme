aa4ec694a75c2b50e933a2c841dd0ad4632bb403
Seraphina Nix <danielnixgibson@gmail.com>
2018-03-22 15:48:04 -0400

    adding a small file I made to learn how to prompt user for input

--- hello-prompt.hs
+++ hello-prompt.hs
@@ -4,4 +4,4 @@ import System.Environment
 main :: IO ()
 main = do putStrLn("Hello, what's your name?")
           name <- getLine
-	  putStrLn("Hi " ++ name ++ "!")
+	  putStrLn("Hi " ++ name ++ "!")	
