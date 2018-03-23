a7391ce8e58b77384a5d3c8e4ea046d0eff1b210
Seraphina Nix <danielnixgibson@gmail.com>
2018-03-23 00:52:14 -0400

    Defined the types with which errors will be handled.

--- Parser.hs
+++ Parser.hs
@@ -2,6 +2,7 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
+import Control.Monad.Error
 import Numeric
 
 main :: IO ()
@@ -224,3 +225,33 @@ atomToString (Atom a) = String a
 
 stringToAtom :: LispVal -> LispVal
 stringToAtom (String s) = Atom s
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected
+                                          ++ " args; found values "
+                                          ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                          ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parser error at " ++ show parseErr
+
+instance Show LispError where show = showError
+instance Error LispError where
+  noMsg = Default "An error has occured"
+  strMsg = Default
+
+type ThrowsError = Either LispError
+
+trapError action = catchError action (return . show)
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
