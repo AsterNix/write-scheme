2c61914e6bb18aeabd2a6ed7efe3b97e024bb303
Seraphina Nix <danielnixgibson@gmail.com>
2018-03-22 20:25:23 -0400

    Basic evaluation is done, recusively computes arithmetic expressions.

--- Parser.hs
+++ Parser.hs
@@ -5,16 +5,15 @@ import Control.Monad
 import Numeric
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn (readExpr (args !! 0))
+main = getArgs >>= print . eval . readExpr . head
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~"
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found " ++ show val
+  Left err -> String $ "No match: " ++ show err
+  Right val -> val
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -172,3 +171,41 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+--comment out the block to make everything not a 
+{-unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                           if null parsed
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n-}
+unpackNum _ = 0
+
+testAtom :: LispVal -> LispVal
+testAtom (Atom _) = Bool True
+testAtom _ = Bool False
+
+testString :: LispVal -> LispVal
+testString (String _) = Bool True
+testString _ = Bool False
+
+testNumber :: LispVal -> LispVal
+testNumber (Number _) = Bool True
+testNumber _ = Bool False
